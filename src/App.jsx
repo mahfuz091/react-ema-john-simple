554a4a37f5aed9bdc528d1690b381376fae8097f
mahfuz091 <engmahfuz14@gmail.com>
2023-03-26 23:05:30 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div id='root'>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default App;
